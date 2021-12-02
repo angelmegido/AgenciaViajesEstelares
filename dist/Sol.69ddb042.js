5aa3738916e19bec3ebe4587b59d2d58b136ec8b
Angel <angelmegido@hotmail.com>
2021-12-02 21:29:30 +0100

    initial commit

---
+++ dist/Sol.69ddb042.js
@@ -0,0 +1,65 @@
+window.confirmacion = function confirmacion() {
+    var pregunta = confirm("¿Deseas visitar la Tierra?");
+    if (pregunta) {
+        alert("Te envío allí a la velocidad de la luz");
+        window.location = "https://es.wikipedia.org/wiki/Tierra";
+    } else alert("Quizás en otro momento...\n Gracias de todas formas");
+};
+window.confirmacionMarte = function confirmacionMarte() {
+    var pregunta = confirm("¿Deseas visitar Marte?");
+    if (pregunta) {
+        alert("Te envío a la velocidad de la luz");
+        window.location = "https://es.wikipedia.org/wiki/Marte_(planeta)";
+    } else alert("Quizás en otro momento...\n Gracias de todas formas");
+};
+window.confirmacionMercurio = function confirmacionMercurio() {
+    var pregunta = confirm("¿Deseas visitar Mercurio?");
+    if (pregunta) {
+        alert("Te envío a la velocidad de la luz");
+        window.location = "https://es.wikipedia.org/wiki/Mercurio_(planeta)";
+    } else alert("Quizás en otro momento...\n Gracias de todas formas");
+};
+window.confirmacionVenus = function confirmacionVenus() {
+    var pregunta = confirm("¿Deseas visitar Venus?");
+    if (pregunta) {
+        alert("Te envío allí a la velocidad de la luz");
+        window.location = "https://es.wikipedia.org/wiki/Venus_(planeta)";
+    } else alert("Quizás en otro momento...\n Gracias de todas formas");
+};
+window.confirmacionJupiter = function confirmacionJupiter() {
+    var pregunta = confirm("¿Deseas visitar Jupiter?");
+    if (pregunta) {
+        alert("Te envío allí a la velocidad de la luz");
+        window.location = "https://es.wikipedia.org/wiki/J%C3%BApiter_(planeta)";
+    } else alert("Quizás en otro momento...\n Gracias de todas formas");
+};
+window.confirmacionSaturno = function confirmacionSaturno() {
+    var pregunta = confirm("¿Deseas visitar Saturno?");
+    if (pregunta) {
+        alert("Te envío allí a la velocidad de la luz");
+        window.location = "https://es.wikipedia.org/wiki/Saturno_(planeta)";
+    } else alert("Quizás en otro momento...\n Gracias de todas formas");
+};
+window.confirmacionUrano = function confirmacionUrano() {
+    var pregunta = confirm("¿Deseas visitar Urano?");
+    if (pregunta) {
+        alert("Te envío allí a la velocidad de la luz");
+        window.location = "https://es.wikipedia.org/wiki/Urano_(planeta)";
+    } else alert("Quizás en otro momento...\n Gracias de todas formas");
+};
+window.confirmacionNeptuno = function confirmacionNeptuno() {
+    var pregunta = confirm("¿Deseas visitar Neptuno?");
+    if (pregunta) {
+        alert("Te envío allí a la velocidad de la luz");
+        window.location = "https://es.wikipedia.org/wiki/Neptuno_(planeta)";
+    } else alert("Quizás en otro momento...\n Gracias de todas formas");
+};
+window.confirmacionSol = function confirmacionSol() {
+    var pregunta = confirm("¿Deseas visitar el Sol?");
+    if (pregunta) {
+        alert("Te envío allí a la velocidad de la luz");
+        window.location = "https://es.wikipedia.org/wiki/Sol";
+    } else alert("Quizás en otro momento...\n Gracias de todas formas");
+};
+
+//# sourceMappingURL=Sol.69ddb042.js.map
